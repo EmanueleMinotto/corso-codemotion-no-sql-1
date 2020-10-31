25510bf639118c365238e6bcb15564df81232c85
Emanuele Minotto <minottoemanuele@gmail.com>
2020-10-31 14:14:39 +0100

    fix

--- 06-sql-join/join.sql
+++ 06-sql-join/join.sql
@@ -2,7 +2,7 @@
 select c.phone, m.message from contacts c join messages m on m.contact_id = c.id;
 
 -- contatti che non hanno mandato messaggi
-select c.id, c.phone from contacts c left join messages m on m.message is null;
+select c.id, c.phone from contacts c left join messages m on c.id = m.contact_id where m.id is null;
 
 -- ultimi 10 messaggi
 select c.phone, m.message from contacts c join messages m on m.contact_id = c.id order by m.id desc limit 10;
